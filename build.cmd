6a79a8fe60898d81160fea3fc40116390fcb1104
ねこ 白い <shiroineko.tfs@gmail.com>
2024-03-04 22:32:45 +0700

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -5,6 +5,7 @@ md .bin
 md .cmake
 
 cd .cmake
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="../.bin" -DCMAKE_BUILD_TYPE=Release ..
-cmake --build .
-cmake --install .
+::cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="../.bin" -DCMAKE_BUILD_TYPE=Release ..
+cmake -SE:"%~dp0" -Be:"./" -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE "-DCMAKE_C_COMPILER:FILEPATH=gcc.exe" "-DCMAKE_CXX_COMPILER:FILEPATH=g++.exe" -G "Unix Makefiles" ..
+make -j4 --debug
+make DESTDIR="../.bin" install
