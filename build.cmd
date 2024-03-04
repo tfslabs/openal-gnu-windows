fd267132e42f1e17bb17436ade7bbb42ba126bb0
ねこ 白い <shiroineko.tfs@gmail.com>
2024-03-04 22:42:58 +0700

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -5,7 +5,6 @@ md .bin
 md .cmake
 
 cd .cmake
-::cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="../.bin" -DCMAKE_BUILD_TYPE=Release ..
-cmake -SE:"%~dp0" -Be:"./" -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE "-DCMAKE_C_COMPILER:FILEPATH=gcc.exe" "-DCMAKE_CXX_COMPILER:FILEPATH=g++.exe" -G "Unix Makefiles" ..
-make -j4 --debug
-make DESTDIR="../.bin" install
+cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX="../.bin" -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE ..
+ninja
+ninja install
